846c10412d4ac46e3073070431cb1ea07d76fb19
Matt Hurst <hurstmatthew13@gmail.com>
2024-01-15 14:43:36 -0500

    added test to check for a successful or not successful login.
    still need to add templates for successful login and need to add handling for unsuccessful logins. does work with tested username and password userid='root' and password='root'.

--- dbapp.py
+++ dbapp.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('dbsignin.html')
+
+@app.route('/process_form', methods=["POST"])
+def process_form():
+
+    userid = request.form['userid']
+    password = request.form['password']
+
     # format for the logins is (id, userid, password) as follows:
     # id is an auto-incremented integer primary key,
     # userid is a not null varchar(255) field for a user's username which should be their name
@@ -13,12 +21,17 @@ def home():
 
     cursor = conn.cursor()
 
-    # Do DB interactions here
+    cursor.execute("SELECT * FROM users WHERE userid=? AND password=?" , (userid, password))
+    result = cursor.fetchone()
 
-    conn.commit()
+    if result:
+        #successfully found the user
+        return "successfully logged in!"
+    else:
+        #did not find the user
+        return "failed log in!"
+
     conn.close()
-
-    return render_template('dbsignin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
