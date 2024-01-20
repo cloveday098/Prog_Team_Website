23fa901f2c5a313468ecaa3a1e3db143e304929a
Loveday, Chance <lovedaycb@ornl.gov>
2024-01-20 11:40:29 -0500

    More styling

---
+++ problems.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Problems</title>
+        <link rel="stylesheet" href="../static/styles/style.css">
+        <script src="https://kit.fontawesome.com/29ba4da398.js" crossorigin="anonymous"></script>
+        <link rel="icon" type="image/x-icon" href="../static/images/mcLogo.png">
+    </head>
+
+    <body>
+
+    </body>
+</html>
