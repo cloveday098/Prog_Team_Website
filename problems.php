ac7f1468f2d02c1a89db801ac2555fcb497681ca
Loveday, Chance <lovedaycb@ornl.gov>
2024-01-22 16:44:02 -0500

    Adjusted the sizes of the student pics to maintain consistent display dimensions; Resized Contact Form popup screen

--- problems.php
+++ problems.php
@@ -8,6 +8,11 @@
     </head>
 
     <body>
-
+    <section id="navbar" class="navbar">
+        <!--Navbar-->
+        <pre>
+            <a href="https://www.maryvillecollege.edu/" target="_blank" ><img align="left" src="../static/Images/mcLogo.png" style="margin-top:-1%; margin-left: 4%;" loading="lazy"></a>    <a class="navlinks" href="about.html">About</a>    <a class="navlinks" href="major.html">Major</a>   <a class="navlinks" href="" onclick="window.open('http://localhost/contact.php', 'newwindow', 'width=1000,height=550'); return false;">Contact</a>    <a class="navlinks" href="http://localhost/problems.php">Problems</a> <a href="./home.html"><img align="right" src="../static/Images/placeholderlogo.png" style="margin-top:-1%; margin-right: 4%;" loading="lazy"></a>
+        </pre>
+    </section>
     </body>
 </html>
